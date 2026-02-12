af25a5c95fef0a6da3b80ac8e72da405c95365cc
unknown <rahmadarifinsusilo17@gmail.com>
2026-02-12 10:40:57 +0700

    login direct to home

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
                 strategy: 'email_code'
             });
 
-            navigate('/verify-login');
+            navigate('/');
         } catch (err) {
             console.error(err);
             setError(err.errors?.[0]?.message || 'Login gagal');
