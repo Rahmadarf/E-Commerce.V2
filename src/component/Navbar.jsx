a5af67dc571aa1e0196473d31119aae9de6b02f3
unknown <rahmadarifinsusilo17@gmail.com>
2026-02-12 10:10:47 +0700

    notify

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -76,7 +76,7 @@ const Navbar = () => {
 
                         {/* Account */}
                         <Tooltip title="Account" arrow>
-                            <Link to="/account" className="text-gray-600 hover:text-blue-700 p-1">
+                            <Link onClick={() => showNotification('Fitur belum tersedia', 'error')} to="/account" className="text-gray-600 hover:text-blue-700 p-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                 </svg>
